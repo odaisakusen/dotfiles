7a4f9c8edc39eb3c02e444c77f0c22a4f58459e7
odaisakusen <odaisakusen@gmail.com>
2021-10-10 15:30:56 -0500

    qtile, alacritty

---
+++ config.py
@@ -0,0 +1,216 @@
+import os
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+from libqtile.utils import guess_terminal
+
+mod = "mod4"
+# terminal = guess_terminal()
+terminal = "alacritty"
+iceberg = {
+    "red": "#e27878",
+    "yellow": "#e2a478",
+    "green": "#b4be84",
+    "cyan": "#89b8c2",
+    "blue": "#84a0c6",
+    "violet": "#a093c7",
+    "light": "#c6c8d1",
+    "dark": "#161821"
+}
+
+keys = [
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "space", lazy.layout.next(),
+        desc="Move window focus to other window"),
+
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawn("jgmenu_run"),
+        desc="Spawn a command using a prompt widget"),
+]
+
+groups = [Group(i) for i in [" ", "爵 ", " ", " ", "5"]]
+
+for i, group in enumerate(groups):
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], str(i + 1), lazy.group[group.name].toscreen(),
+            desc="Switch to group {}".format(group.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], str(i + 1), lazy.window.togroup(group.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(group.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+layouts = [
+    # layout.Columns(border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=4),
+    # layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    layout.MonadTall(
+        border_normal=iceberg["dark"],
+        border_focus=iceberg["red"],
+        border_width=3,
+        margin=8
+    ),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='JetbrainsMono Nerd Font Medium',
+    fontsize=14,
+    padding=5,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    borderwidth=0,
+                    highlight_method="line",
+                    highlight_color=[iceberg["red"]],
+                    block_highlight_text_color="#ffffff",
+                    inactive=iceberg["light"],
+                    active=iceberg["red"]
+                ),
+                widget.Spacer(8),
+                widget.WindowName(),
+                widget.Chord(
+                    chords_colors={
+                        'launch': ("#ff0000", "#ffffff"),
+                    },
+                    name_transform=lambda name: name.upper(),
+                ),
+                widget.Systray(),
+                widget.Clock(format='%A, %B %d  %H:%M'),
+                widget.QuickExit(default_text=" ", countdown_format="{}"),
+            ],
+            28,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
+
+on_start = [
+    "xrandr -s \"1440x900\"",
+    "feh --bg-fill /home/od/index.jpeg",
+    "compton &"
+    #"picom --no-vsync &" # no-vsync for virtualbox
+]
+
+for cmd in on_start:
+    os.system(cmd)
+
+def change_transparency(window):
+    """change window transparency based on his name/type."""
+    kls = window.window.get_wm_class()[1].lower()
+    logger.debug("Change transparency for window: %s", kls)
+    if 'urxvt' in kls:
+        window.cmd_opacity(.7)
+    elif 'firefox' in kls or 'chromium' in kls:
+        window.cmd_opacity(.9)
+    else:
+        if window.floating and 'urxvt' not in kls:
+            # all floating are almost transparent by default (for popups windows)
+            window.cmd_opacity(.95)
+        else:
+            window.cmd_opacity(.8)
+
+
+@hook.subscribe.client_new
+def transparent_window(window):
+    """Make new windows a little transparent."""
+    change_transparency(window)
